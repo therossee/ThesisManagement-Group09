fb02a0bd71cd821189fe8809d5011bbef205ec7e
lucabubi <luca.barbato@studenti.polito.it>
2023-11-08 18:52:27 +0100

    Prototype Frontend Structure + Responsive Sidebar

---
+++ font-end/src/MainLayout.jsx
@@ -0,0 +1,35 @@
+import { Layout } from 'antd';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import NotFound from './routes/NotFound'
+import SideBar from './components/SideBar';
+import TopBar from './components/TopBar';
+import './css/style.css';
+
+const { Content, Footer } = Layout;
+
+function MainLayout() {
+
+    return (
+        <Router>
+            <Layout>
+                <SideBar />
+                <Layout >
+                    <TopBar />
+                    <Content className="content-style">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/*" element={<NotFound />} />
+                            </Routes>
+                    </Content>
+                    <Footer className="footer-style">
+                        SE2 - Group 9
+                    </Footer>
+                </Layout>
+            </Layout>
+        </Router>
+    )
+}
+
+export default MainLayout;
+
---
+++ font-end/src/components/TopBar.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "antd";
+import '../css/style.css';
+
+const { Header } = Layout;
+
+function TopBar() {
+
+    return (
+        <Header className="header-style" />
+    )
+}
+
+export default TopBar;
