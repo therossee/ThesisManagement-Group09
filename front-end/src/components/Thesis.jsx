610ccac49a8fcdddcef610e23c398aaec1a7c11a
Pancasx <100451251@alumnos.uc3m.es>
2023-11-10 19:16:45 +0100

    Form review finished

--- front-end/src/components/Thesis.jsx
+++ front-end/src/components/Thesis.jsx
@@ -1,143 +1,196 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { DatePicker, Button, Form, Input, Select, Steps } from "antd";
 
-const OPTIONS_COSUPERVISORS = ['Apples', 'Nails', 'Bananas', 'Helicopters'];
+const { Option } = Select;
+
+const OPTIONS_COSUPERVISORS = ["Apples", "Nails", "Bananas", "Helicopters"];
 
 const steps = [
-    {
-        title: 'Insert Thesis Proposal',
-        content: <InsertBody />,
-    },
-    {
-        title: 'Review Proposal',
-        content: 'Second-content',
-    },
-    {
-        title: 'Upload',
-        content: 'Last-content',
-    },
+  {
+    title: "Insert Thesis Proposal",
+    content: <InsertBody />,
+  },
+  {
+    title: "Review Proposal",
+    content: <ReviewProposal />,
+  },
+  {
+    title: "Upload",
+    content: <Done />,
+  },
 ];
 
 function InsertThesisProposal() {
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
 
-    const items = steps.map((item) => ({
-        key: item.title,
-        title: item.title,
-    }));
+  const navigate = useNavigate();
+  const [current, setCurrent] = useState(0);
+  const [formData, setFormData] = useState({});
 
-    const navigate = useNavigate();
-    const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
 
-    const next = () => {
-        setCurrent(current + 1);
-    };
-    const prev = () => {
-        setCurrent(current - 1);
-    };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
 
+  const saveFormData = (data) => {
+    setFormData(data);
+    next();
+  };
 
-    return (<>
-        <Steps current={current} items={items} style={{ paddingRight: "20%", paddingLeft: "20%" }} />
-        <div style={{ marginLeft: "15%", marginRight: "15%", marginTop: "3%" }}>
-            <div>
-                {steps[current].content}
-            </div>
-            <div>
-                {current < steps.length - 1 && (
-                    <Button type="primary" onClick={() => next()}>
-                        Next
-                    </Button>
-                )}
-                {current === steps.length - 1 && (
-                    <Button type="primary" onClick={() => navigate("/")}>
-                        Done
-                    </Button>
-                )}
-                {current === 1 && (
-                    <Button
-                        style={{
-                            margin: '0 8px',
-                        }}
-                        onClick={() => prev()}
-                    >
-                        Previous
-                    </Button>
-                )}
-            </div>
+  return (
+    <>
+      <Steps
+        current={current}
+        items={items}
+        style={{ paddingRight: "20%", paddingLeft: "20%" }}
+      />
+      <div style={{ marginLeft: "15%", marginRight: "15%", marginTop: "3%" }}>
+        <div>
+          {current === 0 ? (
+            <InsertBody onSave={saveFormData} />
+          ) : (
+            React.cloneElement(steps[current].content, { formData })
+          )}
         </div>
-    </>
-    )
-}
-
-function InsertBody() {
-
-    const [selectedItems, setSelectedItems] = useState([]);
-    const optionsCoSupervisors = OPTIONS_COSUPERVISORS.filter((x) => !selectedItems.includes(x));
-
-    function disabledDate(current) {
-        // Can not select days before today and today
-        return current && current.valueOf() < Date.now();
-      }
-
-    return (
-        <Form layout="horizontal">
-            <Form.Item label="Title">
-                <Input />
-            </Form.Item>
-            <Form.Item label="Co-Supervisors">
-                <Select
-                    mode="multiple"
-                    placeholder="Select Co-Supervisor"
-                    value={selectedItems}
-                    onChange={setSelectedItems}
-                    options={optionsCoSupervisors.map((x) => ({
-                        value: x,
-                        label: x,
-                    }))}
-                />
-            </Form.Item>
-            <Form.Item label="Keywords">
-                <Input />
-            </Form.Item>
-            <Form.Item label="Type">
-                <Input />
-            </Form.Item>
-            <Form.Item label="Description">
-                <Input.TextArea rows={6} />
-            </Form.Item>
-            <Form.Item label="Required Knowledge">
-                <Input.TextArea rows={4} />
-            </Form.Item>
-            <Form.Item label="Notes">
-                <Input.TextArea rows={4} />
-            </Form.Item>
-            <Form.Item label="Expiration Date">
-                <DatePicker disabledDate={disabledDate}/>
-            </Form.Item>
-            <Form.Item
-                name="level"
-                label="Level"
+        <div>
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+              Next
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button type="primary" onClick={() => navigate("/")}>
+              Done
+            </Button>
+          )}
+          {current === 1 && (
+            <Button
+              style={{
+                margin: "0 8px",
+              }}
+              onClick={() => prev()}
             >
-                <Select
-                    placeholder="Select a option"
-                    allowClear
-                >
-                    <Option value="bachelor">L - Bachelor Degree</Option>
-                    <Option value="master">LM - Master Degree</Option>
-                </Select>
-            </Form.Item>
-            <Form.Item name="cds" label="CdS">
-                    <Select
-                        placeholder="Select a option"
-                        allowClear>
-                            <Option value="Scienze delle merendine">Scienze delle merendine</Option>
-                            <Option value="Università della vita">Università della vita</Option>
-                    </Select>
-            </Form.Item>
-        </Form>
-
-    )
+              Previous
+            </Button>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export { InsertThesisProposal }
+function InsertBody({ onSave }) {
+  const [selectedItems, setSelectedItems] = useState([]);
+  const optionsCoSupervisors = OPTIONS_COSUPERVISORS.filter(
+    (x) => !selectedItems.includes(x)
+  );
+  const [form] = Form.useForm();
+
+  function disabledDate(current) {
+    return current && current.valueOf() < Date.now();
+  }
+
+  const onFinish = (values) => {
+    //Function to save the values of the form when the user clicks save
+    onSave(values);
+  };
+
+  return (
+    <Form form={form} layout="horizontal" onFinish={onFinish}>
+      <Form.Item label="Title" name="title">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Co-Supervisors" name="coSupervisors">
+        <Select
+          mode="multiple"
+          placeholder="Select Co-Supervisor"
+          value={selectedItems}
+          onChange={setSelectedItems}
+          options={optionsCoSupervisors.map((x) => ({
+            value: x,
+            label: x,
+          }))}
+        />
+      </Form.Item>
+      <Form.Item label="Keywords" name="keywords">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Type" name="type">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Description" name="description">
+        <Input.TextArea rows={6} />
+      </Form.Item>
+      <Form.Item label="Required Knowledge" name="requiredKnowledge">
+        <Input.TextArea rows={4} />
+      </Form.Item>
+      <Form.Item label="Notes" name="notes">
+        <Input.TextArea rows={4} />
+      </Form.Item>
+      <Form.Item label="Expiration Date" name="expirationDate">
+        <DatePicker disabledDate={disabledDate} />
+      </Form.Item>
+      <Form.Item name="level" label="Level">
+        <Select placeholder="Select a option" allowClear>
+          <Option value="bachelor">L - Bachelor Degree</Option>
+          <Option value="master">LM - Master Degree</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item name="cds" label="CdS">
+        <Select placeholder="Select a option" allowClear>
+          <Option value="Scienze delle merendine">
+            Scienze delle merendine
+          </Option>
+          <Option value="Università della vita">Università della vita</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Save and Continue
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+function ReviewProposal({ formData }) {
+  //Function to retrieve the data of the form inserted in insert body
+  return (
+    <div>
+      <h2>Review Proposal</h2>
+      <p>Title: {formData.title}</p>
+      <p>Co-Supervisors: {formData.coSupervisors.join(", ")}</p>
+      <p>Keywords: {formData.keywords}</p>
+      <p>Type: {formData.type}</p>
+      <p>Description: {formData.description}</p>
+      <p>Required Knowledge: {formData.requiredKnowledge}</p>
+      <p>Notes: {formData.notes}</p>
+      <p>
+        Expiration Date:{" "}
+        {formData.expirationDate
+          ? formData.expirationDate.format("YYYY-MM-DD")
+          : ""}
+      </p>
+      <p>Level: {formData.level}</p>
+      <p>CdS: {formData.cds}</p>
+    </div>
+  );
+}
+
+function Done() {
+  return (
+    <div>
+      <h2>Done</h2>
+      <p>You have finished!</p>
+    </div>
+  );
+}
+
+export { InsertThesisProposal };
