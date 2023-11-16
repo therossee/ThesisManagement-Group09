56d522dab83a5a5108dda94169d291464d7c8953
Daniele De Rossi <daniele.dr00@gmail.com>
2023-11-16 15:38:07 +0100

    Added student name/professor last name on homepage

--- front-end/src/routes/Home.jsx
+++ front-end/src/routes/Home.jsx
@@ -6,13 +6,13 @@ const { Title, Paragraph } = Typography;
 
 function Home() {
 
-    const  { isLoggedIn, isTeacher } = useAuth();
+    const  { isLoggedIn, isTeacher, user } = useAuth();
 
     return (
         <>
         <Title className="home-title">Welcome to Thesis Management System!</Title>
-        {(isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome professor! You can insert a new proposal or take a look for all the application requests.</Paragraph>}
-        {(isLoggedIn && !isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome student! You can insert a new application request or take a look for all the proposals.</Paragraph>}
+        {(isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome professor {user.surname}! You can insert a new proposal or take a look for all the application requests.</Paragraph>}
+        {(isLoggedIn && !isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome {user.name}! You can insert a new application request or take a look for all the proposals.</Paragraph>}
         {(!isLoggedIn) && <Paragraph style={{paddingLeft: "2%"}}>Welcome guest! Start with logging in.</Paragraph>}
         </>
     )
