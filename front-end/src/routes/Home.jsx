0e403952df45cedad84ad21e96cbb20e594fe68a
Daniele De Rossi <daniele.dr00@gmail.com>
2023-11-16 15:35:33 +0100

    Added text on homepage

--- front-end/src/routes/Home.jsx
+++ front-end/src/routes/Home.jsx
@@ -1,12 +1,20 @@
 import { Typography } from 'antd';
+import { useAuth } from '../App';
 import '../css/style.css';
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 function Home() {
 
+    const  { isLoggedIn, isTeacher } = useAuth();
+
     return (
-        <Title className="home-title">Welcome</Title>
+        <>
+        <Title className="home-title">Welcome to Thesis Management System!</Title>
+        {(isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome professor! You can insert a new proposal or take a look for all the application requests.</Paragraph>}
+        {(isLoggedIn && !isTeacher) && <Paragraph style={{paddingLeft: "2%"}}>Welcome student! You can insert a new application request or take a look for all the proposals.</Paragraph>}
+        {(!isLoggedIn) && <Paragraph style={{paddingLeft: "2%"}}>Welcome guest! Start with logging in.</Paragraph>}
+        </>
     )
 }
 
