b76dd6e2618c2035f950e50c668b016b4dd45ef1
lucabubi <luca.barbato@studenti.polito.it>
2023-11-28 06:59:11 +0100

    Code restyling

--- front-end/src/components/Applications.jsx
+++ front-end/src/components/Applications.jsx
@@ -1,126 +1,129 @@
 import { React, useState, useEffect } from "react";
 import API from "../API";
-import { Divider, List, Skeleton, Avatar, Button, Flex } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { message, Divider, List, Skeleton, Avatar, Button, Flex, Typography, Tooltip } from 'antd';
+import { UserOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
 function ThesisApplications() {
+
+    // List of objects for storing thesis info and applications
     const [data, setData] = useState([]);
-    const [studentApplications, setStudentApplications] = useState({});
-    const [isLoadingTable, setIsLoadingTable] = useState(true);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [buttonsLoading, setButtonsLoading] = useState(false);
+
+    const [dirty, setDirty] = useState(true);
+
+    const { Title } = Typography;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const proposals = await API.getTeacherThesisProposals();
-                setData(proposals);
-                setIsLoadingTable(false);
+                if (dirty) {
+                    setIsLoading(true);
+                    setData([]);
+                    const proposals = await API.getThesisProposals();
 
-                // Carica le richieste degli studenti per ciascuna tesi
-                const studentApplicationsData = {};
-                await Promise.all(
-                    proposals.map(async (proposal) => {
-                        const students = await API.getTeacherThesisApplications(proposal.id);
-                        studentApplicationsData[proposal.id] = Array.isArray(students) ? students : [];
+                    // Load applications for each thesis proposal
+                    proposals.map(async proposal => {
+                        const applications = await API.getTeacherThesisApplications(proposal.id);
+                        if (applications.some(x => x.status === "waiting for approval"))
+                            setData(() => Object.assign([], [
+                                {
+                                    id: proposal.id,
+                                    title: proposal.title,
+                                    applications: applications.filter(x => x.status === "waiting for approval"),
+                                },
+                            ]));
                     })
-                );
-                setStudentApplications(studentApplicationsData);
-            } catch (error) {
-                /* Gestione errore con messaggio */
+                    setIsLoading(false);
+                    setDirty(false);
+                }
+            } catch (err) {
+                message.error(err.message ? err.message : err)
             }
         };
-
         fetchData();
-    }, [isLoadingTable]);
+    }, [dirty]);
+
 
     const acceptApplication = async (proposalId, studentId) => {
-        console.log('Accepting application for proposal_id:', proposalId, 'and student_id:', studentId);
+        setButtonsLoading(true);
         try {
-            setIsLoadingTable(true)
-            const response = await API.acceptThesisApplications(proposalId, studentId);
-            console.log('API response:', response);
-            if (response.success) {
-                setIsLoadingTable(false)
-            } else {
-                const rowElement = document.getElementById(`student-row-${studentId}`);
-                rowElement.classList.add('error-row');
-
-                setTimeout(() => {
-                    rowElement.classList.remove('error-row');
-                }, 1000);
-            }
-        } catch (error) {
-            console.error('API error:', error);
-            const rowElement = document.getElementById(`student-row-${studentId}`);
-            rowElement.classList.add('error-row');
-
-            setTimeout(() => {
-                rowElement.classList.remove('error-row');
-            }, 1000);
+            await API.acceptThesisApplications(proposalId, studentId);
+            message.success("Accepted the application of " + studentId);
+            setDirty(true);
+            setButtonsLoading(false)
+        } catch (err) {
+            message.error(err.message ? err.message : err);
+            setButtonsLoading(false);
         }
     };
+
 
     const rejectApplication = async (proposalId, studentId) => {
-        console.log('Rejecting application for proposal_id:', proposalId, 'and student_id:', studentId);
+        setButtonsLoading(true);
         try {
-            setIsLoadingTable(true)
-            const response = await API.rejectThesisApplications(proposalId, studentId);
-            console.log('API response:', response);
-            if (response.success) {
-                setIsLoadingTable(false)
-            } else {
-                const rowElement = document.getElementById(`student-row-${studentId}`);
-                rowElement.classList.add('error-row');
-
-                setTimeout(() => {
-                    rowElement.classList.remove('error-row');
-                }, 1000);
-            }
-        } catch (error) {
-            console.error('API error:', error);
-            const rowElement = document.getElementById(`student-row-${studentId}`);
-            rowElement.classList.add('error-row');
-
-            setTimeout(() => {
-                rowElement.classList.remove('error-row');
-            }, 1000);
+            await API.rejectThesisApplications(proposalId, studentId);
+            message.success("Rejected the application of " + studentId);
+            setDirty(true);
+            setButtonsLoading(false);
+        } catch (err) {
+            message.error(err.message ? err.message : err);
+            setButtonsLoading(false);
         }
     };
 
-
-
-    return (
-        <>
-            {data.map((item) => (
-                <div key={item.id} style={{marginBottom: "3%"}}>
-                    <Divider orientation="left"><h2>{item.title}</h2></Divider>
+    function ApplicationsList() {
+        return (
+            data.map((x) => (
+                <div key={x.id} >
+                    <Skeleton loading={isLoading} active title={false}>
+                    <Divider orientation="center">
+                            <div style={{ whiteSpace: "normal" }}>
+                                <Title level={4} style={{ margin: "0" }}>
+                                    {x.title}
+                                </Title>
+                            </div>
+                    </Divider>
+                    </Skeleton>
                     <List
-                        className="demo-loadmore-list"
-                        loading={isLoadingTable}
+                        loading={isLoading}
                         itemLayout="horizontal"
-                        dataSource={studentApplications[item.id] || []}
+                        dataSource={x.applications ? x.applications : "no data"}
                         renderItem={(student) => (
-                            <List.Item key={student.id} id={`student-row-${student.id}`} style={{ backgroundColor: student.status === 'accepted' ? 'lightgreen' : 'inherit' }}>
-                                <Skeleton loading={isLoadingTable} avatar title={true} active>
+                            <div style={{ marginRight: "20%", marginLeft: "20%" }}>
+                                <List.Item key={student.id}>
                                     <List.Item.Meta
                                         avatar={<Avatar icon={<UserOutlined />} />}
                                         title={`${student.surname} ${student.name}`}
-                                        description={student.status}
                                     />
-                                    {student.status === 'waiting for approval' ? (
-                                        <Flex wrap="wrap" gap="small">
-                                            <Button onClick={() => acceptApplication(item.id, student.id)} type="primary">Approve</Button>
-
-                                            <Button onClick={() => rejectApplication(item.id, student.id)} danger>Reject</Button>
-                                        </Flex>
-                                    ): null}
-                                </Skeleton>
-                            </List.Item>
+                                    <Flex wrap="wrap" gap="small">
+                                        <Tooltip title="Accept Application">
+                                            <Button loading={buttonsLoading} disabled={buttonsLoading} icon={<CheckOutlined />} onClick={() => acceptApplication(x.id, student.id)} ghost type="primary" />
+                                        </Tooltip>
+                                        <Tooltip title="Reject Application">
+                                            <Button loading={buttonsLoading} disabled={buttonsLoading} icon={<CloseOutlined />} onClick={() => rejectApplication(x.id, student.id)} danger />
+                                        </Tooltip>
+                                    </Flex>
+                                </List.Item>
+                            </div>
                         )}
                     />
                 </div>
-            ))}
-        </>
-    );
+
+            ))
+        )
+    }
+
+    return (
+        data.length > 0 ?
+            <ApplicationsList />
+            :
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Title level={3}>No applications pending..</Title>
+            </div>
+    )
 }
 
 
