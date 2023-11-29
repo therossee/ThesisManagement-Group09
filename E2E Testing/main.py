61b5bc05003101acfbcfdb353db9c4af2147134a
Pancasx <100451251@alumnos.uc3m.es>
2023-11-29 18:54:56 +0100

    First py archive for the test

---
+++ E2E Testing/main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = Options()
+options.add_experimental_option("detach", True) # keep browser open after test
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options) # install chromedriver
+
+driver.get("http://localhost:5173/")
+driver.maximize_window()
+
+
+
+
