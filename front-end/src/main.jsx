fdfb065344c7d5a5636c1dc2d11c0daf7cd47443
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2023-12-08 18:07:03 +0100

    removing Auth0Provider

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react';
 import { AuthProvider } from './components/authentication/useAuth.jsx';
 import App from './App.jsx'
 
@@ -16,19 +15,8 @@ dayjs.extend(Timezone);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="thesis-management-09.eu.auth0.com"
-      clientId="o5I1QNTABwbX6g1xc2lxota9aZQEsOvA"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: 'https://thesis-management-09.eu.auth0.com/api/v2/',
-        scope: 'read:current_user update:current_user_metadata',
-      }}
-      cacheLocation='localstorage'
-    >
       <AuthProvider>
         <App />
       </AuthProvider>
-    </Auth0Provider>
   </React.StrictMode>,
 );
