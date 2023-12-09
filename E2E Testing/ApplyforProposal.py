bb5e7a6642ce2f9e252c2a785dd7f08f4face806
Pancasx <100451251@alumnos.uc3m.es>
2023-12-09 04:06:11 +0100

    ApplyForProposal Automated Test updated

--- E2E Testing/ApplyforProposal.py
+++ E2E Testing/ApplyforProposal.py
@@ -16,28 +16,40 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
 driver.get("http://localhost:5173/")
 driver.maximize_window()
 
-mail_field = driver.find_element(By.XPATH, "//input[@placeholder='Email']")
-mail_field.send_keys("s320213@studenti.polito.it")
+driver.implicitly_wait(5)
+login_button = driver.find_element(By.XPATH, "//span[normalize-space()='Log in']")
+login_button.click();
+print("Login clicked")
+
+driver.implicitly_wait(5)
+user_field = driver.find_element(By.XPATH, "//input[@id='username']")
+user_field.send_keys("s320213@studenti.polito.it")
 print("User inserted")
 
-password_field = driver.find_element(By.XPATH, "//input[@placeholder='Password']")
+driver.implicitly_wait(5)
+password_field = driver.find_element(By.XPATH, "//input[@id='password']")
 password_field.send_keys("s320213")
 print("Password inserted")
 
-login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-login_button.send_keys(Keys.RETURN) # press enter
-print("Login clicked")
+driver.implicitly_wait(5)
+continue_button = driver.find_element(By.XPATH, "//div[@class='cdc80f5fa']")
+continue_button.click();
+print("Continue clicked")
+print("Login successful as a STUDENT")
 
 
+driver.implicitly_wait(5)
 applications_history_button = driver.find_element(By.XPATH, "//span[normalize-space()='Applications History']")
 applications_history_button.click();
 print("applications history clicked")
 
-
+#TEST 1: CHECK ACTUAL STATUS
+driver.implicitly_wait(5)
 status_string = driver.find_element(By.XPATH, "//span[@class='ant-badge-status-text']")
 print(status_string.text)
 
 #Checking there are no applications or only rejected applications
+driver.implicitly_wait(5)
 refresh_status_button = driver.find_element(By.XPATH, "//span[normalize-space()='Refresh List']")
  
 if status_string.text == "accepted":
@@ -50,26 +62,33 @@ elif status_string.text == "rejected":
 else:
     pass
 
+#TEST 2: APPLY FOR A PROPOSAL
 #Click on thesis proposals
+driver.implicitly_wait(5)
 thesis_proposals_button = driver.find_element(By.XPATH, "//span[normalize-space()='Thesis Proposals']")
 thesis_proposals_button.click();
 print("Thesis proposals clicked")
 
 #Click on eye button
+driver.implicitly_wait(5)
 eye_button = driver.find_element(By.CSS_SELECTOR, "tbody tr:nth-child(2) td:nth-child(9) div:nth-child(1) div:nth-child(1) span:nth-child(1)")
 eye_button.click();
 print("Eye clicked")
-
+"""
 #Click on apply button
+driver.implicitly_wait(5)
 apply_button = driver.find_element(By.XPATH, "//button[@class='ant-btn css-dev-only-do-not-override-2i2tap ant-btn-primary ant-btn-background-ghost']")
 apply_button.click();
 print("Apply for proposal clicked")
-
+"""
 #Click on application button
+driver.implicitly_wait(5)
 applications_history_button = driver.find_element(By.XPATH, "//span[normalize-space()='Applications History']")
 applications_history_button.click();
 print("applications history clicked")
 
+driver.implicitly_wait(5)
+status_string = driver.find_element(By.XPATH, "//span[@class='ant-badge-status-text']")
 if status_string.text == "waiting for approval":
     print("Test passed")
 elif status_string.text == "accepted" or status_string.text == "rejected":
