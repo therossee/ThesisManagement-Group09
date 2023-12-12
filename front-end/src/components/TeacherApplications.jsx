108905a456895134e73368000e7551a5f6b9854f
Luca Barbato <luca.barbato@studenti.polito.it>
2023-12-13 00:14:42 +0100

    minor fix

--- front-end/src/components/TeacherApplications.jsx
+++ front-end/src/components/TeacherApplications.jsx
@@ -100,7 +100,7 @@ function TeacherApplications() {
                         dataSource={x.applications}
                         renderItem={(student) => (
                             <List.Item key={student.id}>
-                                <div className="wrapper-enlight" onClick={() => { setStudentInfo(student); setIsOpen(true) }} role="button" >
+                                <div className="wrapper-enlight" onClick={() => {setStudentInfo(student); setIsOpen(true) }} onKeyDown={()=>{}} role="button" >
                                 <List.Item.Meta
                                     avatar={<Avatar icon={<UserOutlined />} />}
                                     style={{padding: ".5%"}}
