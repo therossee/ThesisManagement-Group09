b2a7c906b1bf123d233290de2d5eb30af3c44c1a
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-01-07 22:21:22 +0100

    bug fix

--- front-end/src/components/SideBar.jsx
+++ front-end/src/components/SideBar.jsx
@@ -37,7 +37,7 @@ function SideBar() {
       />
  
       </div>
-      <Menu mode="inline" items={navigation} selectedKeys={[window.location.pathname]} onClick={handleMenuClick} />
+      <Menu className="menu-style" mode="inline" items={navigation} selectedKeys={[window.location.pathname]} onClick={handleMenuClick} />
     </Sider>
   );
 }
