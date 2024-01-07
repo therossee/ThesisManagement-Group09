3c3d7b3b0be2bb14070cfd473154c185a52b8800
Sylvie Molinatto <s318952@studenti.polito.it>
2024-01-07 17:22:38 +0100

    Task/268 upgrade homepage (#133)
    * adding slider with some images to the homepage
    
    * changing sidebar logo
    
    * adding message for user logged on the topbar
    
    * adding icon to login button
    
    * changing logout button
    
    * adding some style rules
    
    * adding image on the homepage
    
    * little change on user text
    
    * mobile homepage updated
    
    * fix style
    
    * center align text
    
    * fixing sonar issues
    
    * fix sonar issues
    
    * reducing duplication for Sonar quality gates

--- front-end/src/components/authentication/LoginButton.jsx
+++ front-end/src/components/authentication/LoginButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import { useAuth } from './useAuth';
+import { UserOutlined } from '@ant-design/icons';
 
 // Functional component with a login button
 const LoginButton = () => {
@@ -8,6 +9,7 @@ const LoginButton = () => {
 
     return (
         <Button ghost type="primary" onClick={() => handleLogin()} style={{ marginTop: "17px" }}>
+            <UserOutlined />
             Log in
         </Button>
     );
