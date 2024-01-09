d96ebfe75905f2842631144ccefa127705dec267
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2024-01-09 21:49:59 +0100

    adding secretary clerk role

--- backend/src/enums/user.js
+++ backend/src/enums/user.js
@@ -1,7 +1,8 @@
 const USER_ROLES = Object.freeze({
     STUDENT: 'student',
     TEACHER: 'teacher',
-    TESTER: 'tester'
+    TESTER: 'tester',
+    SECRETARY_CLERK: 'secretary_clerk',
 });
 
 module.exports = {
