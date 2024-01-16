0771de46c12b033232dfa47974899b7c742bb398
Luca Barbato <luca.barbato@studenti.polito.it>
2024-01-16 19:56:06 +0100

    Sidebar fixing

--- front-end/src/components/TopBar.jsx
+++ front-end/src/components/TopBar.jsx
@@ -10,55 +10,45 @@ const { Header } = Layout;
 
 function TopBar({ collapsed, setCollapsed }) {
 
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isTeacher, userData } = useAuth();
+
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="cover-style" />
       <Header className="header-style">
-        {isAuthenticated ?
-          <IsLoggedInForm collapsed={collapsed} setCollapsed={setCollapsed} />
-          :
-          <div style={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-            <LoginButton />
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {collapsed ?
+              <MenuUnfoldOutlined
+                style={{ fontSize: '26px', marginRight: '20px' }}
+                onClick={() => setCollapsed(!collapsed)}
+              />
+              :
+              <MenuFoldOutlined style={{ fontSize: '26px', marginRight: '20px' }}
+                onClick={() => setCollapsed(!collapsed)} />
+            }
+            {isAuthenticated && (
+              <>
+                <Avatar size="large" style={{ backgroundColor: '#1677ff', marginRight: "10px" }} icon={<UserOutlined />} />
+                <span>{userData.name}</span>
+              </>
+            )}
           </div>
-        }
+          <div>
+            {isTeacher && (
+              <FileAddOutlined
+                style={{ fontSize: '26px', verticalAlign: 'middle', marginRight: '20px' }}
+                onClick={() => navigate('/insert-proposal')}
+              />
+            )}
+            {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+          </div>
+        </div>
       </Header >
     </>
   )
 }
-
-function IsLoggedInForm({ collapsed, setCollapsed }) {
-  const { isTeacher, userData } = useAuth();
-  const navigate = useNavigate();
-
-  return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        {collapsed ?
-          <MenuUnfoldOutlined
-            style={{ fontSize: '26px', verticalAlign: 'middle', marginRight: '20px' }}
-            onClick={() => setCollapsed(!collapsed)}
-          />
-          :
-          <MenuFoldOutlined style={{ fontSize: '26px', verticalAlign: 'middle', marginRight: '20px' }}
-            onClick={() => setCollapsed(!collapsed)} />
-        }
-        <Avatar size="large" style={{ backgroundColor: '#1677ff', marginRight: "10px" }} icon={<UserOutlined />} />
-        <span>{userData.name}</span>
-      </div>
-      <div>
-        {isTeacher && (
-          <FileAddOutlined
-            style={{ fontSize: '26px', verticalAlign: 'middle', marginRight: '20px' }}
-            onClick={() => navigate('/insert-proposal')}
-          />
-        )}
-        <LogoutButton />
-      </div>
-    </div>
-  );
-}
-
 
 export default TopBar;
