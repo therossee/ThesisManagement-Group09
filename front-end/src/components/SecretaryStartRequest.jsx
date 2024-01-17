196d12ca534d0489aaf63c538cc2980dc1c5f98e
micheleschiavone00 <micheleschiavone00@gmail.com>
2024-01-17 23:19:16 +0100

    improved interface

--- front-end/src/components/SecretaryStartRequest.jsx
+++ front-end/src/components/SecretaryStartRequest.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { message, List, Avatar, Button, Flex, Typography, Tooltip, Modal, Badge } from 'antd';
+import { message, List, Avatar, Button, Flex, Typography, Tooltip, Modal, Badge, Card } from 'antd';
 import { CheckOutlined, CloseOutlined, ExclamationCircleFilled, UserOutlined } from '@ant-design/icons';
 import dayjs from "dayjs";
 import API from "../API";
+const { Meta } = Card;
 
 
 function Secretary() {
@@ -89,91 +90,113 @@ function Secretary() {
                         itemLayout="horizontal"
                         dataSource={data}
                         renderItem={(request) => (
-                            <List.Item key={request.id}>
-                                <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                                    <div style={{ flex: 1 }}>
-                                        <List.Item.Meta
-                                            avatar={<Avatar style={{ backgroundColor: '#1677ff' }} icon={<UserOutlined />} size="large" />}
+                            <>
+                                <Badge.Ribbon color={request.status.includes("accepted") ? "green" : (request.status.includes("rejected") ? "red" : "grey")} text={request.status}>
+                                    <Card key={request.id}>
+                                            <Meta
+                                                    avatar={<Avatar style={{ backgroundColor: '#1677ff' }} icon={<UserOutlined />} size="large" />}
+                                                    title={request.student.surname + " " + request.student.name + " (" + request.student.id + ") "}
+                                                    description={
+                                                        <>
+                                                            <div style={{ marginTop: "-5px" }}>
+                                                                {"Creation Date: " + dayjs(request.creation_date).format('DD-MM-YYYY HH:mm')}
+                                                                {request.approvation_date && <div><strong>{"Approvation Date: "}</strong>{dayjs(request.approvation_date).format('DD-MM-YYYY HH:mm')}</div>}
+                                                            </div>
+                                                        </>
+                                                    }
+                                                />
+                                            
+                                        <p><strong>{"Thesis Title: "}</strong>{request.title}</p>
+                                        <p><strong>{"Description: "}</strong>{request.description}</p>
+                                        <p><strong>{"Supervisor: "}</strong></p>
+                                        <Meta
+                                            avatar={<Avatar style={{ backgroundColor: '#b62f2f' }} icon={<UserOutlined />} size="small" />}
                                             style={{ padding: ".5%" }}
-                                            title={request.student.surname + " " + request.student.name + " (" + request.student.id + ") "}
+                                            title={request.supervisor.surname + " " + request.supervisor.name + " (" + request.supervisor.id + ") "}
                                             description={
-                                                <div style={{
-                                                    textAlign: "left",
-                                                    marginTop: "-5px",
-                                                    color: request.status.includes("accepted") ? "green" : (request.status.includes("rejected") ? "red" : "inherit")
-                                                }}>
-                                                    <Badge color={request.status.includes("accepted") ? "green" : (request.status.includes("rejected") ? "red" : "grey")} text={request.status} />
-                                                </div>
-                                            }
+                                                <>
+                                                    <div style={{ marginTop: "-1%" }}>
+                                                        {request.supervisor.cod_department + " • " + request.supervisor.cod_group}
+                                                    </div>
+                                                </>}
                                         />
-                                    </div>
-                                    <div style={{ flex: 1, textAlign: 'right' }}>
-                                        <div><strong>{"Creation Date: "}</strong>{dayjs(request.creation_date).format('DD-MM-YYYY HH:mm')}</div>
-                                        {request.approvation_date && <div><strong>{"Approvation Date: "}</strong>{dayjs(request.approvation_date).format('DD-MM-YYYY HH:mm')}</div>}
-                                    </div>
-                                </div>
-                                <strong>{"Thesis Title: "}</strong>{request.title}
+                                        {request.co_supervisors.length == 0 ? null :
+                                            <p>
+                                                <strong>{"Cosupervisors: "}</strong>
+                                                {request.co_supervisors.map((co_supervisor) => (
+
+                                                    <Meta
+                                                        avatar={<Avatar style={{ backgroundColor: '#3CB371' }} icon={<UserOutlined />} size="small" />}
+                                                        style={{ padding: ".5%" }}
+                                                        title={co_supervisor.surname + " " + co_supervisor.name + " (" + co_supervisor.id + ") "}
+                                                    />
+
+
+
+                                                ))}
+                                            </p>
+                                        }
+                                        <Flex wrap="wrap" gap="small" style={{ padding: ".5%", alignItems: 'center', paddingTop: '2%' }}>
+                                            <Tooltip title="Accept Start Request">
+                                                <Button ghost type="primary"
+                                                    loading={buttonsLoading}
+                                                    disabled={buttonsLoading || request.status !== "waiting for approval"}
+                                                    icon={<CheckOutlined />}
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        showModal(
+                                                            <div>
+                                                                <Paragraph>
+                                                                    <Text strong>
+                                                                        Are you sure you want to accept this start request?
+                                                                    </Text>
+                                                                </Paragraph>
+                                                                <Paragraph>
+                                                                    <Text strong>Thesis title: </Text><Text>{request.title}</Text>
+                                                                    <br />
+                                                                    <Text strong>Student: </Text><Text>{request.student.name + " " + request.student.surname}</Text>
+                                                                </Paragraph>
+                                                            </div>,
+                                                            () => acceptStartRequest(request.id, request.student),
+                                                            "Yes, accept the start request",
+                                                            "Cancel"
+                                                        )
+                                                    }}
+                                                >ACCEPT</Button>
+                                            </Tooltip>
+                                            <Tooltip title="Reject Start Request">
+                                                <Button ghost danger
+                                                    loading={buttonsLoading}
+                                                    disabled={buttonsLoading || request.status !== "waiting for approval"}
+                                                    icon={<CloseOutlined />}
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        showModal(
+                                                            <div>
+                                                                <Paragraph>
+                                                                    <Text strong>
+                                                                        Are you sure you want to reject this start request?
+                                                                    </Text>
+                                                                </Paragraph>
+                                                                <Paragraph>
+                                                                    <Text strong>Thesis title: </Text><Text>{request.title}</Text>
+                                                                    <br />
+                                                                    <Text strong>Student: </Text><Text>{request.student.name + " " + request.student.surname}</Text>
+                                                                </Paragraph>
+                                                            </div>,
+                                                            () => rejectStartRequest(request.id, request.student),
+                                                            "Yes, reject the start request",
+                                                            "Cancel"
+                                                        )
+                                                    }}
+                                                >REJECT</Button>
+                                            </Tooltip>
+                                        </Flex>
+                                    </Card>
+                                </Badge.Ribbon>
                                 <br />
-                                <strong>{"Description: "}</strong>{request.description}
-                                <br />
-                                <strong>{"Supervisor: "}</strong>{request.supervisor.surname + " " + request.supervisor.name}
-                                <Flex wrap="wrap" gap="small" style={{ padding: ".5%", alignItems: 'center', paddingTop: '2%' }}>
-                                    <Tooltip title="Accept Start Request">
-                                        <Button ghost type="primary"
-                                            loading={buttonsLoading}
-                                            disabled={buttonsLoading || request.status !== "waiting for approval"}
-                                            icon={<CheckOutlined />}
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                showModal(
-                                                    <div>
-                                                        <Paragraph>
-                                                            <Text strong>
-                                                                Are you sure you want to accept this start request?
-                                                            </Text>
-                                                        </Paragraph>
-                                                        <Paragraph>
-                                                            <Text strong>Thesis title: </Text><Text>{request.title}</Text>
-                                                            <br />
-                                                            <Text strong>Student: </Text><Text>{request.student.name + " " + request.student.surname}</Text>
-                                                        </Paragraph>
-                                                    </div>,
-                                                    () => acceptStartRequest(request.id, request.student),
-                                                    "Yes, accept the start request",
-                                                    "Cancel"
-                                                )
-                                            }}
-                                        >ACCEPT</Button>
-                                    </Tooltip>
-                                    <Tooltip title="Reject Start Request">
-                                        <Button ghost danger
-                                            loading={buttonsLoading}
-                                            disabled={buttonsLoading || request.status !== "waiting for approval"}
-                                            icon={<CloseOutlined />}
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                showModal(
-                                                    <div>
-                                                        <Paragraph>
-                                                            <Text strong>
-                                                                Are you sure you want to reject this start request?
-                                                            </Text>
-                                                        </Paragraph>
-                                                        <Paragraph>
-                                                            <Text strong>Thesis title: </Text><Text>{request.title}</Text>
-                                                            <br />
-                                                            <Text strong>Student: </Text><Text>{request.student.name + " " + request.student.surname}</Text>
-                                                        </Paragraph>
-                                                    </div>,
-                                                    () => rejectStartRequest(request.id, request.student),
-                                                    "Yes, reject the start request",
-                                                    "Cancel"
-                                                )
-                                            }}
-                                        >REJECT</Button>
-                                    </Tooltip>
-                                </Flex>
-                            </List.Item>
+
+                            </>
                         )}
                     />
                 </div>
